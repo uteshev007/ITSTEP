63b040070a9c9be0aebbcd23bb4c10dadea3c261
uteshev007 <101507848+uteshev007@users.noreply.github.com>
2022-11-22 16:58:03 +0600

    Add files via upload
    HW1 is done

---
+++ HW2/Source.cpp
@@ -0,0 +1,172 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+
+			Задачи по алгоритмам
+
+	1. Вычисление площади круга
+		1.1 Обьъявляем переменную S
+		1.2 Обьъявляем переменную R
+		1.3 Просим ввести значение R
+		1.4 Прописываем формулу вычисления S= π * R(2)
+		1.5 Вывод результата S
+
+	2. Алгоритм перехода перекрестка.
+		2.1 Определяем тип перекрестка регулируемый/нерегулируемый
+		2.2 Если регулируемый жмем на кнопку
+		2.3 Ждем разрешительного сигнала
+		2.4 Убедиться что все автомобили остановились
+		2.5 Перейти на противоположную сторону
+
+		2.1.1 Если нерегулируемый смотрим сперва в левую сторону
+		2.1.2 Убеждаемся что авто левой стороны все остановились
+		2.1.3 Переходим до середины полосы
+		2.1.4 Смотрим в правую сторону
+		2.1.5 Убеждаемся что авто правой стороны все остановились
+		2.1.6 Переходим дальше.
+
+
+	3. Алгоритм снятие денег через терминал с карточки
+		3.1 Находим банкомат
+		3.2 Дойди до банкомата
+		3.3 Взять карточку в руки
+		3.4 Правильной стороной засунуть в картоприемник
+		3.5 Выбрать язык обслуживания
+		3.6 Выбрать функцию снятия наличных
+		3.7 Выбрать сумму снятия или ввести вручную
+		3.8 Ввести пин код
+		3.9 Взять деньги
+		3.10 Взять карточку.
+
+	*/
+
+	// 4 Задача.
+
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)179 << "\t\tVrema dna\t\t   ", cout << (char)179;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)179 << "\tUtro\t  ", cout << (char)179, cout << "\tDen\t", cout << (char)179, cout << "  Vecher  ", cout << (char)179;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+
+	cout << "\n" << "\n" << "\n" << "\n" << "\n";
+
+
+	// 5 Задача.
+
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)179 << "\t\t\tVremena goda\t\t\t\t ", cout << (char)179;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)179 << "\tZima   ", cout << (char)179, cout << "\tVesna\t", cout << (char)179, cout << "  \tLeto\t  ", cout << (char)179, cout << "\tOsen\t ", cout << (char)179;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+
+	cout << "\n" << "\n" << "\n" << "\n" << "\n";
+
+	//6 Задача
+	setlocale(LC_ALL, "RUS");
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " Символ  ", cout << (char)166, cout << " Описание\t\t\t      ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\n\t  ", cout << (char)166, cout << " Перевод каретки на новую строку   ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\r\t  ", cout << (char)166, cout << " Возврат каретки на начало строки  ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\a\t  ", cout << (char)166, cout << " Подача звукового сигнала, beep    ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\b\t  ", cout << (char)166, cout << " Забой символа слева\t\t      ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\t\t  ", cout << (char)166, cout << " Табуляция \t\t\t      ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\\\ \t  ", cout << (char)166, cout << " Вывод символа \\\t\t      ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\'\t  ", cout << (char)166, cout << " Вывод одинарной кавычки \'\t      ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	cout << (char)166, cout << " \\\"\t  ", cout << (char)166, cout << " Вывод двойной кавычки \"\t      ", cout << (char)166;
+	cout << "\n";
+	cout << (char)43 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45;
+	cout << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)45 << (char)43;
+	cout << "\n";
+	
+	cout << "\n" << "\n" << "\n" << "\n" << "\n";
+	// Конец
+
+}
